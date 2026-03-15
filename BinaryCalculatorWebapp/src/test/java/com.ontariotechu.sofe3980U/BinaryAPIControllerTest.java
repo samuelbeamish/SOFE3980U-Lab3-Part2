83a07020dac00d3b1dd8a982b67f9afcd4b083f1
samuelbeamish <samuel.beamish@ontariotechu.net>
2026-03-15 16:30:24 -0400

    Update BinaryAPIControllerTest.java

--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryAPIControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryAPIControllerTest.java
@@ -48,4 +48,76 @@ public class BinaryAPIControllerTest {
 			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(10001))
 			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
     }
+
+	@Test
+	public void multiply() throws Exception {
+		this.mvc.perform(get("/multiply").param("operand1","111").param("operand2","10"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("1110"));
+	}
+
+	@Test
+	public void multiplyJson() throws Exception {
+		this.mvc.perform(get("/multiply_json").param("operand1","111").param("operand2","10"))
+			.andExpect(status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value("111"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value("10"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value("1110"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+	}
+
+	@Test
+	public void and() throws Exception {
+		this.mvc.perform(get("/and").param("operand1","111").param("operand2","1010"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("10"));
+	}
+
+	@Test
+	public void andJson() throws Exception {
+		this.mvc.perform(get("/and_json").param("operand1","111").param("operand2","1010"))
+			.andExpect(status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value("111"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value("1010"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value("10"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));
+	}
+
+	@Test
+	public void or() throws Exception {
+		this.mvc.perform(get("/or").param("operand1","111").param("operand2","1010"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("1111"));
+	}
+
+	@Test
+	public void orJson() throws Exception {
+		this.mvc.perform(get("/or_json").param("operand1","111").param("operand2","1010"))
+			.andExpect(status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value("111"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value("1010"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value("1111"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+	}
+
+	@Test
+	public void multiplyWithZero() throws Exception {
+		this.mvc.perform(get("/multiply").param("operand1","0").param("operand2","101010"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("0"));
+	}
+
+	@Test
+	public void andWithInvalidOperand() throws Exception {
+		this.mvc.perform(get("/and").param("operand1","10x1").param("operand2","1010"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("0"));
+	}
+
+	@Test
+	public void orWithLeadingZeros() throws Exception {
+		this.mvc.perform(get("/or").param("operand1","0011").param("operand2","0100"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("111"));
+	}
 }
